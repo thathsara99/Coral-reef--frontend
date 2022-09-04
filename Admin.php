020166c93b8630e3fb6544eeae5503034d214f10
SamithaPrabath <samithaprabath513@gmail.com>
2022-09-04 18:29:26 +0530

    last edit

--- Admin.php
+++ Admin.php
@@ -64,17 +64,17 @@
                 ><span>Account</span> <i class="bi bi-chevron-down"></i
               ></a>
               <ul>
-                <li><a href="Register.php">Register</a></li>
-                <!-- <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
-                <ul>
-                  <li><a href="#">Deep Drop Down 1</a></li>
-                  <li><a href="#">Deep Drop Down 2</a></li>
-                  <li><a href="#">Deep Drop Down 3</a></li>
-                  <li><a href="#">Deep Drop Down 4</a></li>
-                  <li><a href="#">Deep Drop Down 5</a></li>
-                </ul>
-              </li> -->
-                <li><a href="Login.php">Login</a></li>
+              <li><a href="Admin-users-profile.php">Profile</a></li>
+            <!-- <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
+              <ul>
+                <li><a href="#">Deep Drop Down 1</a></li>
+                <li><a href="#">Deep Drop Down 2</a></li>
+                <li><a href="#">Deep Drop Down 3</a></li>
+                <li><a href="#">Deep Drop Down 4</a></li>
+                <li><a href="#">Deep Drop Down 5</a></li>
+              </ul>
+            </li> -->
+            <li><a href="Logout.php">Logout</a></li>
                 <!-- <li><a href="#">Drop Down 3</a></li>
               <li><a href="#">Drop Down 4</a></li> -->
               </ul>
