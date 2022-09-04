bd9d826ce0e997fc9784ffb233ab647d0c04efe5
SamithaPrabath <samithaprabath513@gmail.com>
2022-09-04 12:35:34 +0530

    First Edit

---
+++ database_connect.php
@@ -0,0 +1,10 @@
+<?php
+    $conn=new mysqli("localhost","root","","coralbarrer");
+    
+    if($conn->connect_error){
+        die("Connection failed ".$conn->connect_error);
+    }
+    // else{
+    //     echo "Connection Success";
+    // }
+?>
